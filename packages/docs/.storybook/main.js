6959d1839569be64a0c54f374612581cf37e67c4
hugo <hugovarellaa@gmail.com>
2023-01-08 20:25:32 -0300

    Documentação do Design system

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,15 +1,15 @@
 module.exports = {
-  stories: ["../src/stories/*.stories.mdx", "../src/stories/*.stories.tsx"],
+  stories: ['../src/pages/*.stories.mdx', '../src/stories/*.stories.tsx'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  framework: "@storybook/react",
+  framework: '@storybook/react',
   core: {
-    builder: "@storybook/builder-vite",
+    builder: '@storybook/builder-vite',
   },
   features: {
     storyStoreV7: true,
   },
-};
+}
