e0296130eb204c77e8c52f8669d3026e0912a809
hugo <hugovarellaa@gmail.com>
2023-01-08 17:06:48 -0300

    exportando Button

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,16 +1,8 @@
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
   backgroundColor: '$ignite500',
   borderRadius: '$md',
   height: '$4',
 })
-
-export function App() {
-  return (
-    <div>
-      <Button>Hello world</Button>
-    </div>
-  )
-}
