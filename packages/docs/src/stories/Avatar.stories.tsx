b376a96e8626e00206dd5d9d00e14d61a69a6d73
hugo <hugovarellaa@gmail.com>
2023-01-09 11:57:30 -0300

    Components Avatar

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/Hugovarellaa.png',
+    alt: 'Hugo Alves Varella',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,3 +1,4 @@
+export * from './components/Avatar/index'
 export * from './components/Box'
 export * from './components/Heading'
 export * from './components/Text'
