eb99f009361e09fa76a5543cb260cbb7d70a5c1d
hugo <hugovarellaa@gmail.com>
2023-01-10 11:10:50 -0300

    Controles do components

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/Hugovarellaa.png',
     alt: 'Hugo Alves Varella',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
