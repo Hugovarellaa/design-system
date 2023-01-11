a4e8c931a04a78f1f6c940a946eb28e5af5d2bd7
hugo <hugovarellaa@gmail.com>
2023-01-11 18:29:52 -0300

    Component button storiesbook

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,12 +1,22 @@
-import { Button } from '@hugo-ignite-ui/react'
+import { Button, ButtonProps } from '@hugo-ignite-ui/react'
 import { Meta, StoryObj } from '@storybook/react'
 
 export default {
   title: 'Button',
   component: Button,
-  args: {},
-} as Meta
+  args: {
+    children: 'Enviar',
+  },
+} as Meta<ButtonProps>
 
-export const Primary: StoryObj = {}
+export const Primary: StoryObj<ButtonProps> = {
+  args: {
+    size: 'small',
+  },
+}
 
-export const Secondary: StoryObj = {}
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    size: 'big',
+  },
+}
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,9 +1,34 @@
+import { ComponentProps } from 'react'
 import { styled } from './styles'
 
 export const Button = styled('button', {
   fontFamily: '$default',
   backgroundColor: '$ignite500',
   borderRadius: '$md',
-  padding: '$4',
-  height: '$4',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
+
+  '&:hover': {
+    backgroundColor: '$ignite300',
+  },
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
+
+export interface ButtonProps extends ComponentProps<typeof Button> {}
