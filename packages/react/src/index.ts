3bbc425a6a32111f436e8200fc727772043d346e
hugo <hugovarellaa@gmail.com>
2023-01-11 13:06:33 -0300

    Testando react

--- packages/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { colors } from '@hugo-ignite-ui/tokens'
-
-console.log(colors)
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,9 @@
+import { colors } from '@hugo-ignite-ui/tokens'
+
+export function App() {
+  return (
+    <div>
+      <h1 style={{ color: colors.ignite300 }}>Olá mundo</h1>
+    </div>
+  )
+}
