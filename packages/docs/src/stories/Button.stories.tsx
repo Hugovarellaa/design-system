c94fe9a7fcecab576f127bbb34365f8e256044b3
hugo <hugovarellaa@gmail.com>
2023-01-12 10:53:09 -0300

    Component Button

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,52 @@
+import { Button, ButtonProps } from '@hugo-ignite-ui/react'
+import { Meta, StoryObj } from '@storybook/react'
+import { ArrowRight } from 'phosphor-react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+  args: {
+    children: 'Send',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {
+  args: {},
+}
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'secondary',
+    children: 'Create new',
+  },
+}
+
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'tertiary',
+    children: 'Cancel',
+  },
+}
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+  },
+}
+
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        Proximo passo
+        <ArrowRight />
+      </>
+    ),
+  },
+}
+
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    disabled: true,
+  },
+}
