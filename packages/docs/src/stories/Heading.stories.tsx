80e5fc4c784d90c22d2ea59c5d5490bb8b47b335
hugo <hugovarellaa@gmail.com>
2023-01-12 09:49:21 -0300

    Documentação storybook Heading

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,19 @@
+import { Heading, HeadingProps } from '@hugo-ignite-ui/react'
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  args: {
+    children: 'Custom title',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 Heading',
+    as: 'h1',
+  },
+}
