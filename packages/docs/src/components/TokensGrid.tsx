e154acb828174478d5f5c2e9f8fefe9e00098b67
hugo <hugovarellaa@gmail.com>
2023-01-12 08:17:30 -0300

    Documentação space

--- packages/docs/src/components/TokensGrid.tsx
+++ packages/docs/src/components/TokensGrid.tsx
@@ -2,15 +2,17 @@ import '../styles/tokens-grid.css'
 
 interface TokensGridProps {
   tokens: Record<string, string>
+  hasRemvalue?: boolean
 }
 
-export function TokensGrid({ tokens }: TokensGridProps) {
+export function TokensGrid({ tokens, hasRemvalue = false }: TokensGridProps) {
   return (
     <table className="tokens-grid">
       <thead>
         <tr>
           <th>Name</th>
           <th>Value</th>
+          {hasRemvalue && <td>Pixels</td>}
         </tr>
       </thead>
       <tbody>
@@ -19,6 +21,9 @@ export function TokensGrid({ tokens }: TokensGridProps) {
             <tr key={name}>
               <td>{name}</td>
               <td>{value}</td>
+              {hasRemvalue && (
+                <td>{Number(value.replace('rem', '')) * 16}px</td>
+              )}
             </tr>
           )
         })}
