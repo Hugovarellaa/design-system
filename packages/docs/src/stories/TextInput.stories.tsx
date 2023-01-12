67e5d41adf95391a173c0f9d5a56fa3db790ac2f
hugo <hugovarellaa@gmail.com>
2023-01-12 11:42:56 -0300

    Documentação do TextInput do storybook

--- packages/docs/src/stories/TextInput.stories.tsx
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -1,10 +1,21 @@
-import { TextInput, TextInputProps } from '@hugo-ignite-ui/react'
+import { Box, Text, TextInput, TextInputProps } from '@hugo-ignite-ui/react'
 import { Meta, StoryObj } from '@storybook/react'
 
 export default {
   title: 'Form/TextInput',
   component: TextInput,
   args: {},
+  decorators: [
+    (Story) => (
+      <Box
+        as="label"
+        css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+      >
+        <Text size="sm">Email address</Text>
+        <Story />
+      </Box>
+    ),
+  ],
 } as Meta<TextInputProps>
 
 export const Primary: StoryObj<TextInputProps> = {
