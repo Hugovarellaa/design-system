c874a31f1759f6637d9ef8fc01d63a63019e50c4
hugo <hugovarellaa@gmail.com>
2023-01-12 11:47:04 -0300

    Component TextArea

---
+++ packages/react/src/components/TextArea.tsx
@@ -0,0 +1,32 @@
+import { ComponentProps } from 'react'
+import { styled } from '../styles'
+
+export const TextArea = styled('textarea', {
+  backgroundColor: '$gray900',
+  padding: '$3 $4',
+  borderRadius: '$sm',
+  boxSizing: 'border-box',
+  border: '2px solid $gray900',
+
+  fontFamily: '$default',
+  fontSize: '$sm',
+  color: '$white',
+  fontWeight: '$regular',
+  resize: 'vertical',
+  minHeight: 80,
+
+  '&:focus': {
+    outline: 0,
+    borderColor: '$ignite300',
+  },
+  '&:disabled': {
+    cursor: 'not-allowed',
+    opacity: 0.5,
+  },
+
+  '&:placeholder': {
+    color: '$gray400',
+  },
+})
+
+export interface TextAreaProps extends ComponentProps<typeof TextArea> {}
